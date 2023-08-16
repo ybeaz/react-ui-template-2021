07fb09c4e7082cb815aff53d3a95d8a5901b9b63
rome_sfba <admin@3440-mbp.gateway.sonic.net>
2023-08-16 14:09:11 -0700

    perf(devops): add ComponentsSamples, XII

--- botBuilding/getBotReactFcScenario.ts
+++ botBuilding/getBotReactFcScenario.ts
@@ -1,10 +1,10 @@
-import { promises as fs } from 'fs'
 import { getReadFileToString } from '../tools/getReadFileToString'
 import { join } from 'path'
 import { consoler } from '../tools/consoler'
 import { getWriteFile } from '../tools/getWriteFile'
 import { getReplacedSpacesInString } from '../tools/getReplacedSpacesInString'
 import { getPromptExample } from '../tools/getPromptExample'
+import { getBotReactFcModel } from './models/getBotReactFcModel'
 
 const SOURCES_DICTIONARY: Record<string, string> = {
   ButtonReactV0101:
@@ -37,78 +37,6 @@ const path03 = SOURCES_DICTIONARY.ToDoListReactV0301
 // const path = SOURCES_DICTIONARY.ToDoListReactV0301test
 
 ;(async () => {
-  type GetBotReactFcDataPropsType = {
-    user01: string
-    assist01: string
-    user02: string
-    assist02: string
-    user03: string
-    assist03: string
-  }
-
-  const getBotReactFcData = ({
-    user01,
-    assist01,
-    user02,
-    assist02,
-    user03,
-    assist03,
-  }: GetBotReactFcDataPropsType): string => {
-    const propmtReturnObject = {
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'system',
-          content:
-            'Context: "NPM ReactJS, Typescript, programming" Language: "Use language based on the User question language" System_message: "Generate a response in the markdown format with typescript code snippet." ',
-        },
-        {
-          role: 'user',
-          content: `Task: Create TypeDoc and a Functional component in ReactJS. Instructions: ${user01}`,
-        },
-        {
-          role: 'assistant',
-          content: assist01,
-        },
-        {
-          role: 'user',
-          content: `Great, nice. This time the task is: Create TypeDoc and a Functional component in ReactJS. Instructions: ${user02}`,
-        },
-        {
-          role: 'assistant',
-          content: assist02,
-        },
-        {
-          role: 'user',
-          content: `Great, nice. This time the task is: Create TypeDoc and a Functional component in ReactJS. Instructions: ${user03}`,
-        },
-        {
-          role: 'assistant',
-          content: assist03,
-        },
-        {
-          role: 'user',
-          content: `Great, nice, I like it. This time the task is: {userText}`,
-        },
-        {
-          role: 'system',
-          content:
-            'System_message: Generate a response in the markdown format with typescript code snippet. Instruction: Replace [@name] with the actual Functional Component name',
-        },
-      ],
-      temperature: 0.1,
-    }
-
-    return JSON.stringify({
-      requestBody: propmtReturnObject,
-      promptExamples: [
-        `Example 1.\\nCreate TypeDoc and a Functional component in ReactJS. Instructions: ${user01}`,
-        `Example 2.\\nCreate TypeDoc and a Functional component in ReactJS. Instructions: ${user02}`,
-        `Example 3.\\nCreate TypeDoc and a Functional component in ReactJS. Instructions: ${user03}`,
-      ],
-    })
-  }
-
   const str01 = await getReadFileToString(path01)
   const user01 = await getPromptExample(str01)
   const assist01 = await getReplacedSpacesInString(str01)
@@ -121,7 +49,7 @@ const path03 = SOURCES_DICTIONARY.ToDoListReactV0301
   const user03 = await getPromptExample(str03)
   const assist03 = await getReplacedSpacesInString(str03)
 
-  const promptReturn = getBotReactFcData({
+  const promptReturn = await getBotReactFcModel({
     user01,
     assist01,
     user02,
@@ -131,9 +59,11 @@ const path03 = SOURCES_DICTIONARY.ToDoListReactV0301
   })
 
   let pathFull = join(__dirname, folderNameOut, fileNameOut)
-  let getWriteFileRes = await getWriteFile(pathFull, promptReturn, {
-    printRes: false,
-  })
+  let getWriteFileRes =
+    promptReturn &&
+    (await getWriteFile(pathFull, promptReturn, {
+      printRes: false,
+    }))
   consoler(
     'getBotReactFcData [124]',
     'getWriteFileRes',
